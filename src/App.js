625d43b9db5e605c23661a4e796d54cc6c0d0cc4
kyphan <51189352+kyphans@users.noreply.github.com>
2022-10-27 14:25:09 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import About from "./About";
+import "./App.css";
+import Blog from "./Blog";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Home from "./Home";
+import Project from "./Project";
+import Service from "./Service";
+
+function App() {
+  console.log('ahihi');
+  return (
+    <div className="App">
+       <Home />
+       <About />
+       <Service />
+       <Project />
+       <Blog />
+       <Contact />
+       <Footer />
+    </div>
+  );
+}
+
+export default App;
