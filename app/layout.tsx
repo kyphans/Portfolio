a5dd3dcd5be89e490a5dc99fc9f062f8b46f2ee1
Ky Phan <kyphanz1@gmail.com>
2024-12-31 14:30:34 +0700

    add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 import Footer from '@/components/Footer'
+import { Head } from 'next/document'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,10 +21,27 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MS773M93KF"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MS773M93KF');
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>
-        <ThemeProvider themes={['pink', 'green']} defaultTheme="pink">
+        <ThemeProvider themes={["pink", "green"]} defaultTheme="pink">
           <Navbar />
-          <main className="pt-16 pb-24">{children}</main>
+          <main className="pb-24 pt-16">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
